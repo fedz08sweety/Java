655fa9825f6a30237d7280de691656c7fa4bc6a9
Sweety Fernandes <156332741+fedz08sweety@users.noreply.github.com>
2024-09-10 15:09:25 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class power
+{
+public static void main(String[] args)
+{
+int base = 3, powerRaised = 4;
+int result = power(base, powerRaised);
+System.out.println(base + "^" + powerRaised + "=" + result);
+}
+public static int power(int base, int powerRaised){
+if (powerRaised != 0){
+return (base * power(base, powerRaised - 1));
+}
+else {
+return 1;
+}
+}
+}
